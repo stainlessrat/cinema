8c11df784cc426ba9d5d461afdcaea9cd59de561
Sergey Stroev <stainlessrat83@gmail.com>
2020-06-06 21:25:26 +0700

    create class main window

--- cinema.py
+++ cinema.py
@@ -1,4 +1,19 @@
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+import sys
 
 
-if __name__ == '__main__':
-    pass
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Cinema")
+        self.setGeometry(350, 100, 700, 500)
+        self.setWindowIcon(QIcon('images.png'))
+
+        self.show()
+
+
+app = QApplication(sys.argv)
+window = Window()
+sys.exit(app.exec_())
